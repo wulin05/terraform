072b35b58e82943b134026935d3afebec3611a2f
linwu <linwu0586@outlook.com>
2026-02-21 17:43:02 +0900

    create terraform s3 bucket

--- project02/main.tf
+++ project02/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_version = ">= 0.12"
+  backend "s3" {
+    bucket = "myapp-bucket"
+    key = "myapp/state.tfstate"
+    region = "ap-northeast-1"
+  }
+}
+
 provider "aws" {
   
 }
